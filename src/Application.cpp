96e1d028244e1cb9b5ffc96c489a4e48a01956d8
felix <felix.lundh@gmail.com>
2021-12-16 20:21:39 +0100

    moved main function to application.cpp

---
+++ src/Application.cpp
@@ -0,0 +1,145 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+const char* windowTitle = "MGN";
+
+void glfwSetWindowCenter(GLFWwindow* window) {
+    // Get window position and size
+    int window_x, window_y;
+    glfwGetWindowPos(window, &window_x, &window_y);
+
+    int window_width, window_height;
+    glfwGetWindowSize(window, &window_width, &window_height);
+
+    // Halve the window size and use it to adjust the window position to the center of the window
+    window_width *= 0.5;
+    window_height *= 0.5;
+
+    window_x += window_width;
+    window_y += window_height;
+
+    // Get the list of monitors
+    int monitors_length;
+    GLFWmonitor **monitors = glfwGetMonitors(&monitors_length);
+
+    if(monitors == NULL) {
+        // Got no monitors back
+        return;
+    }
+
+    // Figure out which monitor the window is in
+    GLFWmonitor *owner = NULL;
+    int owner_x, owner_y, owner_width, owner_height;
+
+    for(int i = 0; i < monitors_length; i++) {
+        // Get the monitor position
+        int monitor_x, monitor_y;
+        glfwGetMonitorPos(monitors[i], &monitor_x, &monitor_y);
+
+        // Get the monitor size from its video mode
+        int monitor_width, monitor_height;
+        GLFWvidmode *monitor_vidmode = (GLFWvidmode*) glfwGetVideoMode(monitors[i]);
+
+        if(monitor_vidmode == NULL) {
+            // Video mode is required for width and height, so skip this monitor
+            continue;
+
+        } else {
+            monitor_width = monitor_vidmode->width;
+            monitor_height = monitor_vidmode->height;
+        }
+
+        // Set the owner to this monitor if the center of the window is within its bounding box
+        if((window_x > monitor_x && window_x < (monitor_x + monitor_width)) && (window_y > monitor_y && window_y < (monitor_y + monitor_height))) {
+            owner = monitors[i];
+
+            owner_x = monitor_x;
+            owner_y = monitor_y;
+
+            owner_width = monitor_width;
+            owner_height = monitor_height;
+        }
+    }
+
+    if(owner != NULL) {
+        // Set the window position to the center of the owner monitor
+        glfwSetWindowPos(window, owner_x + (owner_width * 0.5) - window_width, owner_y + (owner_height * 0.5) - window_height);
+    }
+}
+
+int main(){
+    // glfw: initialize and configure
+    // ------------------------------
+    if(!glfwInit()){
+        printf("Failes to initialize GLFW.");
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, windowTitle, NULL, NULL);
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetWindowCenter(window);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window)){
+        // input
+        // -----
+        processInput(window);
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
